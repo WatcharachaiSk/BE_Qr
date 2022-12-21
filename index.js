99e4219c4c91a2e64243ab03bac4dda3f13ce29f
WatcharachaiSk <watcharachai.sa@rmuti.ac.th>
2022-12-21 16:45:34 +0700

    Add Build

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const path = require("path");
 //use express static folder
 app.use('/api', express.static(path.join(__dirname, '/src/public')))
 
+// Set the path to the dist folder
+const distPath = path.join(__dirname, '/build');
+app.use(express.static(distPath));
+
 app.use(cors());
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: true }));
@@ -15,6 +19,10 @@ const db = require("./src/model/index.model");
 db.sequelize.sync();
 
 app.use("/api", require("./src/routes/index.routes.js"));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'));
+});
 
 app.listen(3500, () => {
   console.log("server is running");
