3b21787b4368d66826539db2d0cf8e088c851dd9
WatcharachaiSk <watcharachai.sa@rmuti.ac.th>
2023-01-21 18:27:02 +0700

    new Build Version 0.0.9

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const username_db = "root";
-// const password_db = "Admin-201118";
-const password_db = "";
+const password_db = "Admin-201118";
+// const password_db = "";
 const host_db = "localhost";
 const database_name = "be_qrcode";
 
